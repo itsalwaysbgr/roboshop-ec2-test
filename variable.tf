2f144f5e11287fbeb89cdbc0dcdff1add8dae9cc
nane <gnane5940@gmail.com>
2025-06-17 23:34:34 +0530

    2025-06-17

--- variable.tf
+++ variable.tf
@@ -1,15 +1,15 @@
-variable "security_group_ids" {
-    default = ["sg-0ccba06af95f41fdb"]
+variable "instance_type" {
+  type        = string
+  default     = "t3.micro"
+  description = "Instance size"
+}
+
+variable "sg_ids" {
+  type        = list(string)
+  description = "List of security group IDs"
 }
 
 variable "tags" {
-    default = {
-        Name = "roboshop-cart"
-        Terraform = "true"
-        Environment = "dev"
-    }
-}
-
-variable "instance_type" {
-    default = "t3.small"
+  type        = map(string)
+  description = "Tags to assign to the instance"
 }
