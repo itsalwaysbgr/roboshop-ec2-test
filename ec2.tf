dfb90f9574113a4a1604287e728c8d2deec898ab
nane <gnane5940@gmail.com>
2025-06-17 23:56:22 +0530

    2025-06-17

--- ec2.tf
+++ ec2.tf
@@ -1,6 +1,7 @@
 module "ec2" {
     source = "../terraform-aws-instance"
-    sg_ids = ["sg-0ea00949004c34cc3"]
+    sg_ids = var.security_group_ids
+    instance_type = "t3.medium"
     tags = {
         Name = "roboshop-cart"
     }
